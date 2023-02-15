376bcda6d36c9e4bff0d73125604596e01354e27
Nitya Kasturey <67405598+NityaKasturey01@users.noreply.github.com>
2023-02-15 19:38:39 +0530

    First Login

--- lib/second_login.dart
+++ lib/second_login.dart
@@ -1,61 +1,17 @@
-import 'dart:convert';
-
-import 'package:apex_society/second_login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
 
-class FirstLogin extends StatefulWidget {
-  const FirstLogin({Key? key}) : super(key: key);
+class SecondLogin extends StatefulWidget {
+  const SecondLogin({Key? key}) : super(key: key);
 
   @override
-  State<FirstLogin> createState() => _FirstLoginState();
+  State<SecondLogin> createState() => _SecondLoginState();
 }
 
-class _FirstLoginState extends State<FirstLogin> {
+class _SecondLoginState extends State<SecondLogin> {
 
   TextEditingController controllerUserID = TextEditingController();
-
-  Future<void> sendData() async {
-
-    if(controllerUserID.text != ""){
-
-    var headers = {
-      'Content-Type': 'application/json',
-      };
-    var request =
-    http.Request('POST', Uri.parse('http://mobileapp.parjinindiainfotech.in/api/Society/LoginSocCd'));
-
-    request.body = json.encode({"SocCd": controllerUserID.text});
-
-    request.headers.addAll(headers);
-    print("req" + request.toString());
-    print("body" + request.body);
-    print("headers" + request.headers.toString());
-
-    http.StreamedResponse response = await request.send();
-
-    Map<String,dynamic> result = {};
-
-    var res1;
-    if (response.statusCode == 200) {
-      //print(await response.stream.bytesToString());
-    print("otp sent succesfully");
-
-      result = jsonDecode(await response.stream.bytesToString()) as Map<String, dynamic>;
-
-      if(result["result"][0]["societyCd"] == controllerUserID.text){
-        Navigator.push(context, MaterialPageRoute(builder: (context) =>
-            SecondLogin()
-          ,),);
-      }
-
-    } else {
-    print(response.reasonPhrase);
-    print("failed");
-    }
-    }
-  }
+  TextEditingController controllerPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +27,7 @@ class _FirstLoginState extends State<FirstLogin> {
                     //Image.asset("assets/images/logo.png", width: 200),
                     SizedBox(height: 100,),
                     Text(
-                      "Apex Society",
+                      "LOGIN",
                       textAlign: TextAlign.center,
                       style: GoogleFonts.nunito(
                         textStyle: TextStyle(
@@ -102,7 +58,59 @@ class _FirstLoginState extends State<FirstLogin> {
                               children: [
                                 Container(
                                   child: Text(
-                                    "Customer ID",
+                                    "User ID",
+                                    textAlign: TextAlign.left,
+                                    style: GoogleFonts.poppins(
+                                      textStyle: TextStyle(
+                                          color: Colors.blueGrey,
+                                          letterSpacing: .2,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w600
+                                      ),
+                                    ),
+                                  ),
+                                  padding: EdgeInsets.only(left: 5),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(left: 6, top: 2, right: 6, bottom: 2),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border(
+                                        bottom: BorderSide(width: 1, color: Colors.lightBlue.shade900),
+                                        top: BorderSide(width: 1, color: Colors.lightBlue.shade900),
+                                        right: BorderSide(width: 1, color: Colors.lightBlue.shade900),
+                                        left: BorderSide(width: 1, color: Colors.lightBlue.shade900),
+                                      ),
+                                      borderRadius: BorderRadius.all(Radius.circular(10))
+                                  ),
+                                  margin: EdgeInsets.only(top: 15),
+                                  child: TextField(
+                                    controller: controllerPassword,
+                                    textAlign: TextAlign.start,
+                                    textAlignVertical: TextAlignVertical.center,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      //contentPadding: EdgeInsets.all(1),
+                                      hintText: 'Enter your User ID',
+                                      prefixIcon: Icon(Icons.person_rounded),
+                                    ),
+                                    style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                        color: Colors.black54,
+                                        letterSpacing: .2,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                SizedBox(height: 30,),
+
+
+                                Container(
+                                  child: Text(
+                                    "Password",
                                     textAlign: TextAlign.left,
                                     style: GoogleFonts.poppins(
                                       textStyle: TextStyle(
@@ -135,7 +143,7 @@ class _FirstLoginState extends State<FirstLogin> {
                                     decoration: InputDecoration(
                                       border: InputBorder.none,
                                       //contentPadding: EdgeInsets.all(1),
-                                      hintText: 'Enter your User ID',
+                                      hintText: 'Enter your Password',
                                       prefixIcon: Icon(Icons.person_rounded),
                                     ),
                                     style: GoogleFonts.lato(
@@ -150,11 +158,11 @@ class _FirstLoginState extends State<FirstLogin> {
                                 ),
 
                                 GestureDetector(child: Container(
-                                  margin: EdgeInsets.only(top: 80),
+                                  margin: EdgeInsets.only(top: 40),
                                   padding: EdgeInsets.symmetric(vertical: 25.0),
                                   width: double.infinity,
                                   child:  TextButton(
-                                    child: Text('NEXT'),
+                                    child: Text('LOGIN'),
                                     style: TextButton.styleFrom(
                                       primary: Colors.white,
                                       backgroundColor: Color.fromRGBO(80, 109, 138, 1),
@@ -163,16 +171,9 @@ class _FirstLoginState extends State<FirstLogin> {
                                     onPressed: () {
                                       print('Pressed');
 
-                                      sendData();
-
-
-
                                     },
                                   ),
                                 ),),
-
-
-
                               ],
                             ),
                           )
